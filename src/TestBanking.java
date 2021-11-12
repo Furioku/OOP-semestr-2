27e83c74793cd3ed1baa1eaa662858859e1d8ad0
Piotr Bobiński <piotr.bobinski@wsb.warszawa.pl>
2021-11-12 11:56:57 +0100

    Pierwszy commit. Klasa testująca i .gitignore

---
+++ src/TestBanking.java
@@ -0,0 +1,13 @@
+/*
+ * This class creates the program to test the banking classes.
+  */
+
+
+public class TestBanking {
+
+  public static void main(String[] args) {
+
+    System.out.println("Welcome to my bank");
+
+  }
+}
